05f001145e23417693c68e09a0bb9a868f301f28
Lesly Jean-Baptiste <lesjba56@yahoo.com>
2020-02-16 17:58:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,4 +4,5 @@ def  introduction (name)
 
 
 def introduction_with_language(name,language)
-  puts "Hi, my name is#{name} and "
+  puts "Hi, my name is#{name} and I am learning #{language}.Ember.js}. "
+end
